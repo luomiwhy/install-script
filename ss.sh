195f243e0d3d18aa953c9890f5dda9ef96539afe
luomiwhy <luomiwhy@gmail.com>
2019-06-02 13:44:42 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,4 +1,9 @@
+yum install -y vim wget net-tools yum-utils
+
 wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
 
 chmod +x shadowsocks-libev.sh
 
+./shadowsocks-libev.sh
+
+firewall-cmd --zone=public --list-ports
