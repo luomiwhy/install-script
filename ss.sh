903be18543a5765ac6ec9096a932a487796a6f4e
luomiwhy <luomiwhy@gmail.com>
2020-02-16 16:03:04 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,22 +1,15 @@
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 timedatectl set-timezone Asia/Shanghai
 
-yum install -y vim wget net-tools yum-utils gcc g++ 
-
-wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
-
+wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
 chmod +x shadowsocks-libev.sh
-
 ./shadowsocks-libev.sh
 
-sed -i 's/\#baseurl/baseurl/g' /etc/yum.repos.d/epel.repo
-sed -i 's/metalink/\#metalink/g' /etc/yum.repos.d/epel.repo
-
-firewall-cmd --zone=public --list-ports
 
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod +x tcp.sh
 ./tcp.sh
+
 
 uname -r
 sysctl net.ipv4.tcp_available_congestion_control
