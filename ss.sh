2d74c77161bfddab9d00767596f3cd03c47ecbc0
luomiwhy <luomiwhy@gmail.com>
2020-11-04 16:02:07 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,14 +1,14 @@
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 timedatectl set-timezone Asia/Shanghai
 
-wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
-chmod +x shadowsocks-libev.sh
-./shadowsocks-libev.sh
-
-
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod +x tcp.sh
 ./tcp.sh
+
+
+wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
+chmod +x shadowsocks-libev.sh
+./shadowsocks-libev.sh
 
 
 uname -r
@@ -20,3 +20,7 @@ lsmod | grep bbr
 
 # tcp加速 kcptun、bbrplus、锐速、dog-tunnel、rescue被抢vps
 # 转发 socat.sh
+
+source <(curl -sL https://git.io/trojan-install)
+# nyy.mgdianzi.xyz
+
