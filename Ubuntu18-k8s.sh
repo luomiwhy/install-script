c710a48640b47ba146d7977f753e9327e06af4aa
luomiwhy <luomiwhy@gmail.com>
2021-01-08 16:53:39 +0800

    Create Ubuntu18-k8s.sh

---
+++ Ubuntu18-k8s.sh
@@ -0,0 +1,113 @@
+#!/bin/bash 
+
+# 更换 源
+cat <<EOF | sudo tee /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+sudo apt-get update
+
+# 主机名设置
+hostnamectl set-hostname k8s-master1
+# 设置机器 ip
+
+# host 配置： 192.168.121.129 k8s-master1
+sudo vim /etc/hosts
+
+# 同步时间
+
+# 关闭 swap
+sudo swapoff -a
+# sudo vim /etc/fstab
+sudo sed -i "s/\/swap/# \/swap/g" /etc/fstab
+
+# 关闭 SeLinux
+setenforce 0
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+
+# 添加网桥过滤
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo modprobe br_netfilter
+sudo sysctl -p /etc/sysctl.d/k8s.conf
+
+# 开启 ipvs
+
+# 安装runtime 如： docker-ce
+# step 1: 安装必要的一些系统工具
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+# step 2: 安装GPG证书
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+# Step 3: 写入软件源信息
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Step 4: 更新并安装 Docker-CE
+sudo apt-get -y update
+sudo apt-get install -y containerd.io=1.2.13-2 docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "registry-mirrors": ["https://registry.cn-hangzhou.aliyuncs.com"],
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker && sudo usermod -aG docker $USER
+
+# 安装 kubelet kubeadm kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-cache madison kubelet kubeadm kubectl |grep 1.20.1-00
+sudo apt-get install -y kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
+# sudo apt-mark hold kubelet kubeadm kubectl
+
+# 准备镜像
+kubeadm config images list
+images=(  # 下面的镜像应该去除"k8s.gcr.io/"的前缀，版本换成上面获取到的版本
+    kube-apiserver:v1.20.1
+    kube-controller-manager:v1.20.1
+    kube-scheduler:v1.20.1
+    kube-proxy:v1.20.1
+    pause:3.2
+    etcd:3.4.13-0
+    coredns:1.7.0
+)
+for imageName in ${images[@]} ; do
+    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
+    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+done
+
+# 初始化Master
+sudo kubeadm init --kubernetes-version=v1.20.1 --apiserver-advertise-address=192.168.121.129 --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# 单节点，设置master节点也可以运行Pod
+kubectl taint nodes --all node-role.kubernetes.io/master-
+# 部署网络插件
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
