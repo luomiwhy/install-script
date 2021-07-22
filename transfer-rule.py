155232273003c683bb1eeccf357f8d745f81ad10
luomiwhy <luomiwhy@gmail.com>
2021-07-22 17:02:49 +0800

    Update transfer-rule.py

--- transfer-rule.py
+++ transfer-rule.py
@@ -3,14 +3,37 @@
 # pip3 install pyyaml
 
 from urllib.request import urlopen
-import yaml
 
-url="https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/direct.txt"
-dic = yaml.safe_load(urlopen(url).read())
+source = [
+    {
+        "url": "https://cdn.jsdelivr.net/gh/Loyalsoldier/surge-rules@release/direct.txt",
+        "diversion": "DIRECT"
+    }
+]
 
-# print(type(dic), dir(dic))
-# print(dic)
 
-for d in dic["payload"]:
-    
+def generateRules(url, diversion) :
+    dic = urlopen(url).read()
+    dic = dic.decode()
+    # print(type(dic), dir(dic))
+    # print(dic)
+    rule = []
+    for d in dic.splitlines():
+        if d.startswith("."):
+            rule.append("DOMAIN-SUFFIX,"+d[1:]+","+diversion)
+        else:
+            rule.append("DOMAIN,"+d+","+diversion)
+
+
+ruleAll = []
+for m in source:
+    ruleAll.extend(generateRules(m["url"], m["diversion"]))
+
+with open("Spectre.rules", "w") as f:
+    f.writelines("[Rule]")
+    f.writelines("DOMAIN-SUFFIX,cn,DIRECT")
+    for r in ruleAll:
+        f.writelines(r)
+    f.writelines("GEOIP,CN,DIRECT")
+    f.writelines("FINAL,PROXY")
 
