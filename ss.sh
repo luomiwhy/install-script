8c9f0c6bf11ff88aa3e9888e792d1b57c0f7329d
luomiwhy <luomiwhy@gmail.com>
2022-11-07 11:06:44 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,9 +1,9 @@
 # os： Debian 8/9
 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-#timedatectl set-timezone Asia/Shanghai
-apt install -y vim mysql-server
-vim /etc/ssh/sshd_config
+# timedatectl set-timezone Asia/Shanghai
+# apt install -y vim mysql-server
+# vim /etc/ssh/sshd_config
 
 
 #https://github.com/ylx2016/Linux-NetSpeed
@@ -27,10 +27,13 @@ source /etc/profile
 go version
 # go install
 
+# https://github.com/klzgrad/naiveproxy
 
+
+
+#---------------------------------------------------------------------------------------------------
 # tcp加速 kcptun、bbrplus、锐速、dog-tunnel、rescue被抢vps
 # 转发 socat.sh
-
 
 mysql -u root -p
 GRANT ALL PRIVILEGES ON *.* TO 'll'@'localhost' IDENTIFIED BY '2wsx3edc' WITH GRANT OPTION;
